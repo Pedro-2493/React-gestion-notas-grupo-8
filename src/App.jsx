b3435eda928999ddb7a36b39d7c1c7b2181b7bb2
kevinn-9 <kevinvelez7797@gmail.com>
2026-04-23 17:31:08 -0500

    feat: envolver App con UsuariosProvider y agregar ruta /usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import Home from "./views/Home";
 import Contacto from "./views/Contacto";
 import Docentes from "./views/Docentes";
 import LoginPage from "./views/LoginPage";
+import UsuariosList from "./views/UsuariosList";
+import { UsuariosProvider } from "./context/UsuariosContext";
 
 const App = () => {
   return (
-    <>
+    <UsuariosProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/docentes" element={<Docentes />} />
+        <Route path="/usuarios" element={<UsuariosList />} />
       </Routes>
       <Footer />
-    </>
+    </UsuariosProvider>
   );
 };
 
